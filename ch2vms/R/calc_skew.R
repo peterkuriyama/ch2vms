4ae1a71645d83f3f27d2ca27ea5155264241a9ec
peterkuriyama <ptrkrym@uw.edu>
2016-12-08 12:09:38 -0800

    add funtion that calculates skew of distribution

---
+++ ch2vms/R/calc_skew.R
@@ -0,0 +1,24 @@
+#' Calculate skew of a catch distribution
+
+#' Function to calculate the skew of a distribution (third moment)
+
+#' @param input Vector of values 
+
+#' @export
+#' @examples
+#' calc_skew(input = rnorm(1000))
+
+calc_skew <- function(input){
+  #Remove NAs
+  
+  nn <- length(input)
+  
+  xhat <- mean(input)
+  num <- nn * sum((input - xhat) ^ 3)
+  denom <- (nn - 1) * (nn - 2) * sd(input) ^ 3
+  skew <- num / denom
+  #Equation is from this website...
+  #http://www.real-statistics.com/descriptive-statistics/symmetry-skewness-kurtosis/
+
+  return(skew)
+}
