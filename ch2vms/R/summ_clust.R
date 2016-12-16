817218b2dce8d77e96ba1381ed060e7dc8a41d04
peterkuriyama <ptrkrym@uw.edu>
2016-12-16 12:05:49 -0800

    update summ_clust function so that it adds everything to input data frame

--- ch2vms/R/summ_clust.R
+++ ch2vms/R/summ_clust.R
@@ -19,29 +19,29 @@ summ_clust <- function(dat){
   # dat <- dat %>% group_by(clust) %>% mutate(ntows = length(unique(haul_id)))
 
   #For each trip
-  trips <- dat %>% group_by(drvid, trip_id, dyear) %>% mutate(trip_hpounds = sum(hpounds),
+  dat <- dat %>% group_by(drvid, trip_id, dyear) %>% mutate(trip_hpounds = sum(hpounds),
     trip_apounds = sum(apounds)) %>% group_by(drvid, trip_id, species, dyear) %>%
-    summarize(trip_hpounds = unique(trip_hpounds), trip_apounds = unique(trip_apounds),
+  mutate(trip_hpounds = unique(trip_hpounds), trip_apounds = unique(trip_apounds),
       trip_spp_hperc = sum(hpounds) / trip_hpounds, 
       trip_spp_aperc = sum(apounds) / trip_apounds) %>% as.data.frame
 
   #For each vessel
-  vessel <- dat %>% group_by(drvid, dyear) %>% mutate(vess_hpounds = sum(hpounds),
+  dat <- dat %>% group_by(drvid, dyear) %>% mutate(vess_hpounds = sum(hpounds),
     vess_apounds = sum(apounds)) %>% group_by(drvid, species, dyear) %>%
-    summarize(vess_hpounds = unique(vess_hpounds), vess_apounds = unique(vess_apounds),
+    mutate(vess_hpounds = unique(vess_hpounds), vess_apounds = unique(vess_apounds),
       vess_spp_hperc = sum(hpounds) / vess_hpounds,
       vess_spp_aperc = sum(apounds) / vess_apounds) %>% as.data.frame
   
   #For each cluster
-  cluster <- dat %>% group_by(clust, dyear) %>% mutate(clust_hpounds = sum(hpounds),
+  dat <- dat %>% group_by(clust, dyear) %>% mutate(clust_hpounds = sum(hpounds),
     clust_apounds = sum(apounds)) %>% group_by(clust, dyear, species) %>%
-    summarize(clust_hpounds = unique(clust_hpounds), clust_apounds = unique(clust_apounds),
+    mutate(clust_hpounds = unique(clust_hpounds), clust_apounds = unique(clust_apounds),
       clust_spp_hperc = sum(hpounds) / clust_hpounds,
       clust_spp_aperc = sum(apounds) / clust_apounds) %>% as.data.frame
       
   
-  out <- list(trips = trips, vessel = vessel, cluster = cluster)
-  return(out)
+  # out <- list(trips = trips, vessel = vessel, cluster = cluster)
+  return(dat)
 }
 
   
